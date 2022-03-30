aae1ae00717afede5d794f6cf83b467f44068df4
tranquoctrung1 <tranquoctrung.12k9@gmail.com>
2022-03-30 10:51:54 +0700

    update code for fake device

--- client/src/components/fakeDevice/fakeDevice.tsx
+++ client/src/components/fakeDevice/fakeDevice.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { RootState } from "../../store/store";
 import { useSelector, useDispatch } from "react-redux";
 import { addAction } from "../../feature/FakeDevice/fakeDeviceSlice";
@@ -14,6 +14,8 @@ const FakeDevice = () => {
   const value = useSelector((state: RootState) => state.fakeDevice.value);
   const hostname = useSelector((state: RootState) => state.hostname.value);
 
+  const [isBusyGetDataFakeDevice, setIsBusyGetDataFakeDevice] = useState(false);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -24,6 +26,7 @@ const FakeDevice = () => {
         .then((res) => {
           if (res.data.length > 0) {
             dispatch(addAction(res.data));
+            setIsBusyGetDataFakeDevice(true);
           }
         })
         .catch((err) => console.log(err.message));
@@ -36,62 +39,85 @@ const FakeDevice = () => {
     <div className="box-table-fake-device">
       <div className="table-fake-device">
         <h3 className="box-form-title">Danh Sách Thiết Bị Khác</h3>
-        <DataTable
-          paginator
-          rows={5}
-          emptyMessage="No devices found."
-          currentPageReportTemplate="Showing {first} to {last} of {totalRecords} devices"
-          paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-          value={value}
-          selectionMode="single"
-          dataKey="_id"
-          responsiveLayout="scroll"
-        >
-          <Column
-            key="_id"
-            field="_id"
-            header="ID"
-            sortable
-            headerStyle={{ width: "5rem" }}
-            bodyStyle={{ textAlign: "center", overflow: "visible" }}
-          ></Column>
-          <Column
-            key="ProvinceId"
-            field="ProvinceId"
-            header="Mã tỉnh"
-            filterField="privince"
-            sortable
-            headerStyle={{ width: "5rem" }}
-            bodyStyle={{ textAlign: "center", overflow: "visible" }}
-          ></Column>
-          <Column
-            key="Serial"
-            field="Serial"
-            headerStyle={{ width: "5rem" }}
-            bodyStyle={{ textAlign: "center", overflow: "visible" }}
-            header="Số Seri"
-            filterField="serial"
-            sortable
-          ></Column>
-          <Column
-            key="SiteId"
-            field="SiteId"
-            headerStyle={{ width: "5rem" }}
-            bodyStyle={{ textAlign: "center", overflow: "visible" }}
-            header="ID điểm"
-            filterField="SiteId"
-            sortable
-          ></Column>
-          <Column
-            key="SiteName"
-            field="SiteName"
-            headerStyle={{ width: "5rem" }}
-            bodyStyle={{ textAlign: "center", overflow: "visible" }}
-            header="Tên điểm"
-            filterField="SiteName"
-            sortable
-          ></Column>
-        </DataTable>
+        {isBusyGetDataFakeDevice && value.length > 0 && (
+          <DataTable
+            paginator
+            rows={15}
+            emptyMessage="No devices found."
+            currentPageReportTemplate="Showing {first} to {last} of {totalRecords} devices"
+            paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+            value={value}
+            selectionMode="single"
+            dataKey="_id"
+            responsiveLayout="scroll"
+            resizableColumns
+          >
+            <Column
+              key="Serial"
+              field="Serial"
+              headerStyle={{ width: "5rem" }}
+              bodyStyle={{ textAlign: "center", overflow: "visible" }}
+              header="Số Seri"
+              filterField="serial"
+              sortable
+            ></Column>
+            <Column
+              key="SiteId"
+              field="SiteId"
+              headerStyle={{ width: "5rem" }}
+              bodyStyle={{ textAlign: "center", overflow: "visible" }}
+              header="ID điểm"
+              filterField="SiteId"
+              sortable
+            ></Column>
+            <Column
+              key="SiteName"
+              field="SiteName"
+              headerStyle={{ width: "5rem" }}
+              bodyStyle={{ textAlign: "center", overflow: "visible" }}
+              header="Tên điểm"
+              filterField="SiteName"
+              sortable
+            ></Column>
+            <Column
+              key="ProvinceId"
+              field="ProvinceId"
+              header="Mã tỉnh"
+              filterField="ProvinceId"
+              sortable
+              headerStyle={{ width: "5rem" }}
+              bodyStyle={{ textAlign: "center", overflow: "visible" }}
+            ></Column>
+            <Column
+              key="ProvinceMame"
+              field="ProvinceMame"
+              header="Tên tỉnh"
+              filterField="ProvinceMame"
+              sortable
+              headerStyle={{ width: "5rem" }}
+              bodyStyle={{ textAlign: "center", overflow: "visible" }}
+            ></Column>
+            <Column
+              key="ViwaterId"
+              field="ViwaterId"
+              header="Mã Viwater"
+              filterField="ViwaterId"
+              sortable
+              headerStyle={{ width: "5rem" }}
+              bodyStyle={{ textAlign: "center", overflow: "visible" }}
+            ></Column>
+
+            <Column
+              key="ViwaterName"
+              field="ViwaterName"
+              header="Tên Viwater"
+              filterField="ViwaterName"
+              sortable
+              headerStyle={{ width: "5rem" }}
+              bodyStyle={{ textAlign: "center", overflow: "visible" }}
+            ></Column>
+          </DataTable>
+        )}
       </div>
     </div>
   );
